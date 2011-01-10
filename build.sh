3da38c8462c284119351d2b06cd820542d2b0607
Tommi Virtanen <tv@hq.newdream.net>
2011-01-10 15:35:11 -0800

    Do the compilation even when tests are not present.
    Be very nice to other users of the machine.

--- build.sh
+++ build.sh
@@ -11,14 +11,14 @@ if [ ! -e Makefile ]; then
 fi
 
 # Actually build the project
-#make || exit 4
+ionice -c3 nice -n20 make -j16 || exit 4
 
 # The "make -q check" probe in build.sh.example is faulty in that
 # screwups in Makefiles make it think there are no unit tests to
 # run. That's unacceptable; use a dumber probe.
 if [ -e src/gtest ]; then
     # run "make check", but give it a time limit in case a test gets stuck
-    ../maxtime 1800 make check || exit 5
+    ../maxtime 1800 ionice -c3 nice -n20 make check || exit 5
 fi
 
 exit 0
