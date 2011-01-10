3c9bc270b8f2c5ea5539b0fd830c44c67b96d7c4
Tommi Virtanen <tv@hq.newdream.net>
2011-01-10 15:42:19 -0800

    Use ccache, to speed up compilation.

--- build.sh
+++ build.sh
@@ -11,14 +11,16 @@ if [ ! -e Makefile ]; then
 fi
 
 # Actually build the project
-ionice -c3 nice -n20 make -j16 || exit 4
+export CCACHE_DIR="$PWD/../.ccache"
+install -d -m0700 "$CCACHE_DIR"
+ionice -c3 nice -n20 make -j16 CC='ccache gcc' CXX='ccache g++' || exit 4
 
 # The "make -q check" probe in build.sh.example is faulty in that
 # screwups in Makefiles make it think there are no unit tests to
 # run. That's unacceptable; use a dumber probe.
 if [ -e src/gtest ]; then
     # run "make check", but give it a time limit in case a test gets stuck
-    ../maxtime 1800 ionice -c3 nice -n20 make check || exit 5
+    ../maxtime 1800 ionice -c3 nice -n20 make check CC='ccache gcc' CXX='ccache g++' || exit 5
 fi
 
 exit 0
