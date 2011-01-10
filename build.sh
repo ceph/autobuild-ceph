13bb137e8bc00f6fd2c7d3c68b7267a240abb2de
Tommi Virtanen <tv@hq.newdream.net>
2011-01-10 15:46:55 -0800

    Make ccache optional.

--- build.sh
+++ build.sh
@@ -11,16 +11,25 @@ if [ ! -e Makefile ]; then
 fi
 
 # Actually build the project
-export CCACHE_DIR="$PWD/../.ccache"
-install -d -m0700 "$CCACHE_DIR"
-ionice -c3 nice -n20 make -j16 CC='ccache gcc' CXX='ccache g++' || exit 4
+
+# clear out any $@ potentially passed in
+set --
+
+# enable ccache if it is installed
+if command -v ccache >/dev/null; then
+  export CCACHE_DIR="$PWD/../.ccache"
+  install -d -m0700 "$CCACHE_DIR"
+  set -- CC='ccache gcc' CXX='ccache g++'
+fi
+
+ionice -c3 nice -n20 make -j16 "$@" || exit 4
 
 # The "make -q check" probe in build.sh.example is faulty in that
 # screwups in Makefiles make it think there are no unit tests to
 # run. That's unacceptable; use a dumber probe.
 if [ -e src/gtest ]; then
     # run "make check", but give it a time limit in case a test gets stuck
-    ../maxtime 1800 ionice -c3 nice -n20 make check CC='ccache gcc' CXX='ccache g++' || exit 5
+    ../maxtime 1800 ionice -c3 nice -n20 make check "$@" || exit 5
 fi
 
 exit 0
