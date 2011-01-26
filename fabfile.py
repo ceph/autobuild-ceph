db3e7a9e80dfce295f2776541e286384504d75b0
Tommi Virtanen <tommi.virtanen@dreamhost.com>
2011-01-26 13:44:01 -0800

    Migrate from setup script to fabfile.

--- fabfile.py
+++ fabfile.py
@@ -45,5 +45,20 @@ def gitbuilder():
     with cd('/srv/autobuild-ceph'):
         sudo('git init')
         sudo('git pull ~ubuntu/bundle master')
-        sudo('./setup autobuild-ceph')
+        if not exists('gitbuilder.git'):
+            sudo('rm -rf gitbuilder.git.tmp')
+            sudo('git clone https://github.com/tv42/gitbuilder.git gitbuilder.git.tmp')
+            with cd('gitbuilder.git.tmp'):
+                sudo('git checkout 4c26550e018bcfe397275153a3b66bf30cb53a84')
+                sudo('ln -s ../build.sh ./')
+                sudo('ln -s ../branches-local ./')
+                sudo('git clone git://ceph.newdream.net/git/ceph.git build')
+                sudo('chown -R autobuild-ceph:autobuild-ceph build out')
+            sudo('mv gitbuilder.git.tmp gitbuilder.git')
+        sudo('install -d -m0755 logs')
+        sudo('install -m0644 /dev/null logs/stdout.log')
+        sudo('install -m0644 /dev/null logs/stderr.log')
+
+        sudo('install --owner=root --group=root -m0644 autobuild-ceph.conf /etc/init/autobuild-ceph.conf')
+    sudo('start autobuild-ceph')
     run('rm bundle')
