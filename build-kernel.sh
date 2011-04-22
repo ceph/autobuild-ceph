0381d1fb248f64b8cacf46a5faad185eb31e7272
Tommi Virtanen <tommi.virtanen@dreamhost.com>
2011-04-22 16:51:27 -0700

    Set up a kernel gitbuilder.
    Let different gitbuilders perform different actions. Use that to autobuild
    ceph-client.git kernels. Result file management is still unfinished.

---
+++ build-kernel.sh
@@ -0,0 +1,75 @@
+#!/bin/sh -x
+set -e
+
+if [ ! -e Makefile ]; then
+    echo "$0: no Makefile, aborting." 1>&2
+    exit 3
+fi
+
+# Actually build the project
+
+# clear out any $@ potentially passed in
+set --
+
+# enable ccache if it is installed
+export CCACHE_DIR="$PWD/../../ccache"
+if command -v ccache >/dev/null; then
+  if [ ! -e "$CCACHE_DIR" ]; then
+    echo "$0: have ccache but cache directory does not exist: $CCACHE_DIR" 1>&2
+  else
+    set -- CC='ccache gcc' CXX='ccache g++'
+  fi
+else
+  echo "$0: no ccache found, compiles will be slower." 1>&2
+fi
+
+install -d -m0755 build~/out
+(
+    cat ../../kernel-config
+    # we really need this to get the packages the way we want them, so just enforce it here
+    echo 'CONFIG_LOCALVERSION_AUTO=y'
+    ) >build~/out/.config
+
+echo "$0: new kernel config options:"
+# listnewconfig was contained in v2.6.36, but it seems out/ignore/*
+# doesn't work quite right to ignore everything before that, so
+# instead just ignore errors coming from it
+ionice -c3 nice -n20 make O=build~/out listnewconfig "$@" || :
+
+echo "$0: running make oldconfig..."
+yes '' | ionice -c3 nice -n20 make O=build~/out oldconfig "$@"
+
+echo "$0: building..."
+# build dir has ~ suffix so it gets ignored by git and doesn't make
+# the source tree look modified (get "+" in version); using subdir out
+# so the debs go to e.g.
+# build~/linux-image-2.6.38-ceph-00020-g4b2a58a_ceph_amd64.deb
+ionice -c3 nice -n20 make O=build~/out LOCALVERSION=-ceph KDEB_PKGVERSION=ceph deb-pkg -j16 "$@" || exit 4
+
+REV="$(git rev-parse HEAD)"
+OUTDIR="../out/tarball/sha1/$REV"
+OUTDIR_TMP="${OUTDIR}.tmp"
+install -d -m0755 -- "$OUTDIR_TMP"
+mv -- build~/*.deb "$OUTDIR_TMP/"
+
+# put our temp files inside .git/ so ls-files doesn't see them
+git ls-files --modified >.git/modified-files
+if [ -s .git/modified-files ]; then
+    rm -f "$TARBALL.tmp"
+    echo "error: Modified files:" 1>&2
+    cat .git/modified-files 1>&2
+    exit 6
+fi
+
+git ls-files --exclude-standard --others >.git/added-files
+if [ -s .git/added-files ]; then
+    rm -f "$TARBALL.tmp"
+    echo "error: Added files:" 1>&2
+    cat .git/added-files 1>&2
+    exit 7
+fi
+
+# we're successful, the files are ok to be published
+mv -- "$OUTDIR.tmp" "$OUTDIR"
+
+exit 0
--- fabfile.py
+++ fabfile.py
@@ -7,6 +7,10 @@ env.roledefs['gitbuilder_ceph'] = [
     'ubuntu@gitbuilder.ceph.newdream.net',
     ]
 
+env.roledefs['gitbuilder_kernel'] = [
+    'ubuntu@gitbuilder-kernel-amd64.ceph.newdream.net',
+    ]
+
 def _apt_install(*packages):
     sudo(' '.join(
             [
@@ -21,33 +25,19 @@ def _apt_install(*packages):
                 ]
             + list(packages)))
 
-@roles('gitbuilder_ceph')
-def gitbuilder_ceph():
+def _gitbuilder(flavor, git_repo, extra_remotes={}, extra_packages=[], ignore=[]):
+    """
+    extra_remotes will be fetch but not autobuilt. useful for tags.
+    """
     # shut down old instance, it exists
     sudo("initctl list|grep -q '^autobuild-ceph\s' && stop autobuild-ceph || :")
     _apt_install(
         'ntp',
         'build-essential',
-        'automake',
-        'libtool',
-        'pkg-config',
-        'libboost-dev',
-        'libedit-dev',
-        'libssl-dev',
-        'libcrypto++-dev',
-        'libgtkmm-2.4-dev',
-        'libfuse-dev',
-        'libexpat1-dev',
-        'libfcgi-dev',
-        'libcurl4-gnutls-dev',
-        'libatomic-ops-dev',
-        'libgoogle-perftools-dev',
-        'libkeyutils-dev',
         'ccache',
         'git',
         'logrotate',
-        'python-pip',
-        'python-virtualenv',
+        *extra_packages
         )
     sudo(
         ' '.join([
@@ -69,6 +59,7 @@ def gitbuilder_ceph():
     with cd('/srv/autobuild-ceph'):
         sudo('git init')
         sudo('git pull ~ubuntu/bundle master')
+        sudo('ln -sf build-{flavor}.sh build.sh'.format(flavor=flavor))
         if not exists('gitbuilder.git'):
             sudo('rm -rf gitbuilder.git.tmp')
             sudo('git clone https://github.com/tv42/gitbuilder.git gitbuilder.git.tmp')
@@ -76,9 +67,22 @@ def gitbuilder_ceph():
                 sudo('git checkout 4c26550e018bcfe397275153a3b66bf30cb53a84')
                 sudo('ln -s ../build.sh ./')
                 sudo('ln -s ../branches-local ./')
-                sudo('git clone git://ceph.newdream.net/git/ceph.git build')
+                sudo('git clone {git_repo} build'.format(git_repo=git_repo))
                 sudo('chown -R autobuild-ceph:autobuild-ceph build out')
             sudo('mv gitbuilder.git.tmp gitbuilder.git')
+        with cd('gitbuilder.git/build'):
+            for name, url in extra_remotes.items():
+                sudo(
+                    'git remote set-url {name} {url} || git remote add {name} {url}'.format(
+                        name=name,
+                        url=url,
+                        ),
+                    user='autobuild-ceph',
+                    )
+        if ignore:
+            sudo('install -d -m0755 --owner=autobuild-ceph --group=autobuild-ceph gitbuilder.git/out/ignore')
+            for sha in ignore:
+                sudo('touch gitbuilder.git/out/ignore/{sha}'.format(sha=sha))
         sudo('install -d -m0755 --owner=autobuild-ceph --group=autobuild-ceph ccache')
         sudo('install -d -m0755 logs')
 
@@ -86,7 +90,49 @@ def gitbuilder_ceph():
     sudo('start autobuild-ceph')
     run('rm bundle')
 
+@roles('gitbuilder_kernel')
+def gitbuilder_kernel():
+    _gitbuilder(
+        flavor='kernel',
+        git_repo='git://ceph.newdream.net/git/ceph-client.git',
+        extra_remotes=dict(
+            linus='git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux-2.6.git',
+            ),
+        extra_packages=[
+            'fakeroot',
+            ],
+        ignore=[
+            'fbeb94b65cf784ed8bf852131e28c9fb5c4c760f',
+            ],
+        )
+
 @roles('gitbuilder_ceph')
+def gitbuilder_ceph():
+    _gitbuilder(
+        flavor='ceph',
+        git_repo='git://ceph.newdream.net/git/ceph.git',
+        extra_packages=[
+            'automake',
+            'libtool',
+            'pkg-config',
+            'libboost-dev',
+            'libedit-dev',
+            'libssl-dev',
+            'libcrypto++-dev',
+            'libgtkmm-2.4-dev',
+            'libfuse-dev',
+            'libexpat1-dev',
+            'libfcgi-dev',
+            'libcurl4-gnutls-dev',
+            'libatomic-ops-dev',
+            'libgoogle-perftools-dev',
+            'libkeyutils-dev',
+            'python-pip',
+            'python-virtualenv',
+            ],
+        )
+
+@roles('gitbuilder_ceph', 'gitbuilder_kernel')
 def gitbuilder_serve():
     _apt_install(
         'thttpd',
