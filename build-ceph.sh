cdce942942671e33eac9bade9175f93e605bbac3
Tommi Virtanen <tommi.virtanen@dreamhost.com>
2011-04-25 14:48:57 -0700

    Move the ceph binary tarballs into using directories with a tarball.
    Now everything can always produce a directory, and work with the same
    logic in run-real.
    
    This needs CEPH_TARBALL_DEFAULT_URL to be changed in ceph-autotest.git.

--- build-ceph.sh
+++ build-ceph.sh
@@ -39,21 +39,21 @@ if [ -e src/gtest ]; then
     ../maxtime 1800 ionice -c3 nice -n20 make check "$@" || exit 5
 fi
 
-TARDIR="../out/tarball/sha1"
-install -d -m0755 -- "$TARDIR"
-MACH="$(uname -m)"
 REV="$(git rev-parse HEAD)"
+OUTDIR="../out/tarball/sha1/$REV"
+OUTDIR_TMP="${OUTDIR}.tmp"
+install -d -m0755 -- "$OUTDIR_TMP"
+MACH="$(uname -m)"
 INSTDIR="inst.tmp"
 [ ! -e "$INSTDIR" ]
 ../maxtime 1800 ionice -c3 nice -n20 make install DESTDIR="$PWD/$INSTDIR"
-TARBALL="$TARDIR/$MACH.$REV.tgz"
-tar czf "$TARBALL.tmp" -C "$INSTDIR" .
+tar czf "$OUTDIR_TMP/ceph.$MACH.tgz" -C "$INSTDIR" .
 rm -rf -- "$INSTDIR"
 
 # put our temp files inside .git/ so ls-files doesn't see them
 git ls-files --modified >.git/modified-files
 if [ -s .git/modified-files ]; then
-    rm -f "$TARBALL.tmp"
+    rm -rf "$OUTDIR_TMP"
     echo "error: Modified files:" 1>&2
     cat .git/modified-files 1>&2
     exit 6
@@ -61,13 +61,19 @@ fi
 
 git ls-files --exclude-standard --others >.git/added-files
 if [ -s .git/added-files ]; then
-    rm -f "$TARBALL.tmp"
+    rm -rf "$OUTDIR_TMP"
     echo "error: Added files:" 1>&2
     cat .git/added-files 1>&2
     exit 7
 fi
 
-# we're successful, the binary tarball is ok to be published
-mv -- "$TARBALL.tmp" "$TARBALL"
+# we're successful, the files are ok to be published; try to be as
+# atomic as possible about replacing potentially existing OUTDIR
+if [ -e "$OUTDIR" ]; then
+    rm -rf -- "$OUTDIR.old"
+    mv -- "$OUTDIR" "$OUTDIR.old"
+fi
+mv -- "$OUTDIR_TMP" "$OUTDIR"
+rm -rf -- "$OUTDIR.old"
 
 exit 0
