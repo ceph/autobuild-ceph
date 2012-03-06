9cfe330212aca630b7ca78588bc718ec85ef068e
Sage Weil <sage@newdream.net>
2012-03-06 10:08:46 -0800

    disable ccache for ceph
    it breaks leveldb :(

--- build-ceph.sh
+++ build-ceph.sh
@@ -26,16 +26,16 @@ fi
 set --
 
 # enable ccache if it is installed
-export CCACHE_DIR="$PWD/../../ccache"
-if command -v ccache >/dev/null; then
-  if [ ! -e "$CCACHE_DIR" ]; then
-    echo "$0: have ccache but cache directory does not exist: $CCACHE_DIR" 1>&2
-  else
-    set -- CC='ccache gcc' CXX='ccache g++'
-  fi
-else
-  echo "$0: no ccache found, compiles will be slower." 1>&2
-fi
+#export CCACHE_DIR="$PWD/../../ccache"
+#if command -v ccache >/dev/null; then
+#  if [ ! -e "$CCACHE_DIR" ]; then
+#    echo "$0: have ccache but cache directory does not exist: $CCACHE_DIR" 1>&2
+#  else
+#    set -- CC='ccache gcc' CXX='ccache g++'
+#  fi
+#else
+#  echo "$0: no ccache found, compiles will be slower." 1>&2
+#fi
 
 ionice -c3 nice -n20 make -j16 "$@" || exit 4
 
