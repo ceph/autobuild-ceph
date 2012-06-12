309e6bb2f87d5b29472931cebff05ed89141255f
Sage Weil <sage@newdream.net>
2012-06-12 13:16:31 -0700

    disable oneiric builders for gcov, notcmalloc

--- fabfile.py
+++ fabfile.py
@@ -12,13 +12,13 @@ env.roledefs['gitbuilder_ceph'] = [
 
 env.roledefs['gitbuilder_ceph_gcov'] = [
 #    'ubuntu@gitbuilder-gcov-amd64.ceph.newdream.net',
-    'ubuntu@gitbuilder-oneiric-gcov-amd64.ceph.newdream.net',
+#    'ubuntu@gitbuilder-oneiric-gcov-amd64.ceph.newdream.net',
     'ubuntu@gitbuilder-precise-gcov-amd64.ceph.newdream.net',
     ]
 
 env.roledefs['gitbuilder_ceph_notcmalloc'] = [
 #    'ubuntu@10.3.14.76',
-    'ubuntu@gitbuilder-oneiric-notcmalloc-amd64.ceph.newdream.net',
+#    'ubuntu@gitbuilder-oneiric-notcmalloc-amd64.ceph.newdream.net',
     'ubuntu@gitbuilder-precise-notcmalloc-amd64.ceph.newdream.net',
     ]
 
