0cc011b15922c351a3b6f4e0786990d971a9d84a
Joe Buck <jbbuck@gmail.com>
2012-11-21 16:21:08 -0800

    Adding the --enable-cephfs-java flag to configure.
    Signed-off-by: Joe Buck <jbbuck@gmail.com>
    Reviewed-by: Dan Mick <dan.mick@newdream.net>

--- build-ceph-gcov.sh
+++ build-ceph-gcov.sh
@@ -13,7 +13,7 @@ echo --START-IGNORE-WARNINGS
 [ ! -x autogen.sh ] || ./autogen.sh || exit 1
 autoconf || true
 echo --STOP-IGNORE-WARNINGS
-[ ! -x configure ] || ./configure --enable-coverage --with-debug --with-radosgw --with-fuse --with-tcmalloc --with-libatomic-ops --with-gtk2 --with-profiler || exit 2
+[ ! -x configure ] || ./configure --enable-coverage --with-debug --with-radosgw --with-fuse --with-tcmalloc --with-libatomic-ops --with-gtk2 --with-profiler --enable-cephfs-java || exit 2
 
 if [ ! -e Makefile ]; then
     echo "$0: no Makefile, aborting." 1>&2
