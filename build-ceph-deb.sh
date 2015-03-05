594ab828cede75a133993ab95922d5c01eb5fbe2
Loic Dachary <ldachary@redhat.com>
2015-03-05 22:17:33 +0100

    Revert "build: use 'git submodule deinit' instead of 'rm -rf'"
    This reverts commit a4323ad08fb909bf216fc3ebe0a25cc67d4c0a6c.

--- build-ceph-deb.sh
+++ build-ceph-deb.sh
@@ -2,11 +2,21 @@
 set -e
 
 git clean -fdx && git reset --hard
-git submodule deinit -f .
+git submodule foreach 'git clean -fdx && git reset --hard'
+rm -rf ceph-object-corpus
+rm -rf ceph-erasure-code-corpus
+rm -rf src/gmock
+rm -rf src/leveldb
+rm -rf src/libs3
+rm -rf src/mongoose
+rm -rf src/civetweb
+rm -rf src/rocksdb
+rm -rf src/erasure-code/jerasure/gf-complete
+rm -rf src/erasure-code/jerasure/jerasure
 rm -rf .git/modules/
 /srv/git/bin/git submodule sync
 /srv/autobuild-ceph/use-mirror.sh
-/srv/git/bin/git submodule update --init --recursive
+/srv/git/bin/git submodule update --init
 git clean -fdx
 
 
