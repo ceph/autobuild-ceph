a32a7ae97b205e265223fa641da6a9a537a6a02d
Sage Weil <sage@redhat.com>
2015-05-29 10:28:38 -0700

    fabfile: kill apache2 builders
    Signed-off-by: Sage Weil <sage@redhat.com>

--- fabfile.py
+++ fabfile.py
@@ -854,14 +854,6 @@ def gitbuilder_ceph_deb_precise_ndn():
                         extra_remotes={'gh': 'git://github.com/ceph/ceph.git'})
     _sync_out_to_dho('ceph-precise', 'emerging@hq.newdream.net')
 
-@roles('gitbuilder_apache2_deb_oneiric')
-def gitbuilder_apache2_deb_oneiric():
-    _deb_builder('git://ceph.newdream.net/git/apache2-2.2.20.git', 'deb')
-    with cd('/srv/autobuild-ceph'):
-        sudo('echo oneiric > dists')
-        sudo('echo apache2 > pkgname')
-    _sync_to_gitbuilder('apache2','deb','basic')
-
 @roles('gitbuilder_modfastcgi_deb_oneiric')
 def gitbuilder_modfastcgi_deb_oneiric():
     _deb_builder('git://ceph.newdream.net/git/libapache-mod-fastcgi-2.4.7.git', 'deb')
@@ -870,14 +862,6 @@ def gitbuilder_modfastcgi_deb_oneiric():
         sudo('echo libapache-mod-fastcgi > pkgname')
     _sync_to_gitbuilder('libapache-mod-fastcgi','deb','basic')
 
-@roles('gitbuilder_apache2_deb_precise')
-def gitbuilder_apache2_deb_precise():
-    _deb_builder('git://ceph.newdream.net/git/apache2-2.2.22.git', 'deb')
-    with cd('/srv/autobuild-ceph'):
-        sudo('echo precise > dists')
-        sudo('echo apache2 > pkgname')
-    _sync_to_gitbuilder('apache2','deb','basic')
-
 @roles('gitbuilder_modfastcgi_deb_precise')
 def gitbuilder_modfastcgi_deb_precise():
     _deb_builder('git://ceph.newdream.net/git/libapache-mod-fastcgi-2.4.7-0910052141.git', 'deb')
@@ -885,10 +869,6 @@ def gitbuilder_modfastcgi_deb_precise():
         sudo('echo precise > dists')
         sudo('echo libapache-mod-fastcgi > pkgname')
     _sync_to_gitbuilder('libapache-mod-fastcgi','deb','basic')
-
-@roles('gitbuilder_apache2_deb_ndn')
-def gitbuilder_apache2_deb_ndn():
-    _ndn_deb_gitbuilder('apache2', 'deb')
 
 @roles('gitbuilder_modfastcgi_deb_ndn')
 def gitbuilder_modfastcgi_deb_ndn():
@@ -924,7 +904,6 @@ def gitbuilder_kernel_ndn():
        'gitbuilder_kernel',
        # dhodeploy
        'gitbuilder_ceph_deb_ndn',
-       'gitbuilder_apache2_deb_ndn',
        'gitbuilder_modfastcgi_deb_ndn',
        'gitbuilder_collectd_deb_ndn',
        'gitbuilder_kernel_ndn',
